7dde85c0d8e20c2cb36bd925aa3784bd0d76a428
Ronyonka <rononyonka@gmail.com>
2019-02-03 13:43:23 +0300

    API key

--- app/views.py
+++ app/views.py
@@ -8,4 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    title = "Home - If it isn't here, it probably didn't happen"
+    return render_template('index.html', title = title)
+
+@app.route('/source/<source_id>')
+def movie(source_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('news.html',id = source_id)
