42f9ea42bc57a51481d35c9aa1926b9fe795c4a6
Ronyonka <rononyonka@gmail.com>
2019-02-03 16:14:41 +0300

    added request

--- app/views.py
+++ app/views.py
@@ -10,8 +10,10 @@ def index():
     View root page function that returns the index page and its data
     '''
 
+    news_sources = get_sources('sources')
+    print(news_sources)
     title = "Home - If it isn't here, it probably didn't happen"
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, sources=news_sources)
 
 @app.route('/source/<source_id>')
 def movie(source_id):
