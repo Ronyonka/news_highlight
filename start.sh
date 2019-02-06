858fce801c73d006af1905010f9ab1a62efddcb0
Ronyonka <rononyonka@gmail.com>
2019-02-06 11:19:01 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY = 'e36d67f02d724927982d7af6a45e8483'
-export SECRET_KEY = 'bumblebee'
+export NEWS_API_KEY ='e36d67f02d724927982d7af6a45e8483'
+export SECRET_KEY='bumblebee'
 
 python3.6 manage.py server
